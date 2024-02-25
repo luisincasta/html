5529af6b08acb47d8d71399802ab953f0a822aa4
luisincasta <161236425+luisincasta@users.noreply.github.com>
2024-02-25 12:05:44 -0500

    Add files via upload

---
+++ Codigo.js
@@ -0,0 +1,9 @@
+let Bienbenido;
+Bienbenido=0
+alert("Bienvenido a CHERRIS Y ABOGADOS")
+let nombre= prompt("Dime tu nombre");
+alert("Bienbenido "+ nombre);
+let Contactanos;
+Contactanos=0
+Number= 9981214919;
+alert("Contactanos al correo jenifer@cherrizlaw.com o al numero: "+ Number + "Nosotros te ayudamos" + nombre ); 
